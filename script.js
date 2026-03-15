aa5eb4e9d35b02471d7c11cc8aee1d45d6b7f7c3
ALGORITHIM574 <githinjidci0437@gmail.com>
2026-03-15 21:05:30 +0300

    JOB TRACKER APP

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.from("h1", {
+    y: -100,
+    opacity: 0,
+    duration: 1.5
+});
+gsap.from("p",{
+x: -200,
+opacity:0,
+duration:5.5
+});
+gsap.from("main",{
+    y:-400,
+    opacity:0,
+    duration:5
+});
